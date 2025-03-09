f8bf460a446e93b64905ab79c57b4b60c8323767
NrioModer <167567724+NrioModer@users.noreply.github.com>
2025-03-09 18:01:14 +0300

    branches

--- addjokers.js
+++ addjokers.js
@@ -199,28 +199,30 @@ let stickers = [
      },
 ]
 
+
 let blinds = [
      {
-       name: "The Bully",
+       name: "The Wall",
        text: [
          "Extra large blind",
          "{C:inactive}({C:red}4x{C:inactive} Base for {C:attention}$$$$${C:inactive})",
          "{C:inactive}(Appears from Ante 2)"
        ],
-       image_url: "img/the_bully.png",
+       image_url: "img/the_wall.png",
        rarity: "Boss Blind"
      },
-  // {
-  //   name: "Violet Vessel",
-  //   text: [
-  //     "Very large blind",
-  //     "{C:inactive}({C:red}6x{C:inactive} Base for {C:attention}$$$$$$$${C:inactive})",
-  //     "{C:inactive}(Appears from Ante 8)"
-  //   ],
-  //   image_url: "img/violet_vessel.png",
-  //   rarity: "Showdown"
-  // },
+     {
+       name: "Violet Vessel",
+       text: [
+         "Very large blind",
+         "{C:inactive}({C:red}6x{C:inactive} Base for {C:attention}$$$$$$$${C:inactive})",
+         "{C:inactive}(Appears from Ante 8)"
+       ],
+       image_url: "img/violet_vessel.png",
+       rarity: "Showdown"
+     },
 ]
+
 
 let shop_items = []
 
