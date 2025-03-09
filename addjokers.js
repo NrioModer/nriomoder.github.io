016f7af66f2b54bfae7289008d0ceeaba6f35450
NrioModer <167567724+NrioModer@users.noreply.github.com>
2025-03-09 17:50:06 +0300

    Update addjokers.js

--- addjokers.js
+++ addjokers.js
@@ -188,7 +188,29 @@ let consumables = []
 
 let card_modifications = []
 
-let decks = []
+
+let blinds = [
+     {
+       name: "The Bully",
+       text: [
+         "Extra large blind",
+         "{C:inactive}({C:red}4x{C:inactive} Base for {C:attention}$$$$${C:inactive})",
+         "{C:inactive}(Appears from Ante 2)"
+       ],
+       image_url: "img/the_bully.png",
+       rarity: "Boss Blind"
+     },
+  // {
+  //   name: "Violet Vessel",
+  //   text: [
+  //     "Very large blind",
+  //     "{C:inactive}({C:red}6x{C:inactive} Base for {C:attention}$$$$$$$${C:inactive})",
+  //     "{C:inactive}(Appears from Ante 8)"
+  //   ],
+  //   image_url: "img/violet_vessel.png",
+  //   rarity: "Showdown"
+  // },
+]
 
 let stickers = [
      {
