9e1fd483ee99a8af08ad29f2cb98ffc5b16b5a74
NrioModer <167567724+NrioModer@users.noreply.github.com>
2025-03-09 18:08:15 +0300

    Update addjokers.js

--- addjokers.js
+++ addjokers.js
@@ -199,7 +199,6 @@ let stickers = [
      },
 ]
 
-
 let blinds = [
      {
        name: "The Wall",
@@ -211,18 +210,7 @@ let blinds = [
        image_url: "img/the_wall.png",
        rarity: "Boss Blind"
      },
-     {
-       name: "Violet Vessel",
-       text: [
-         "Very large blind",
-         "{C:inactive}({C:red}6x{C:inactive} Base for {C:attention}$$$$$$$${C:inactive})",
-         "{C:inactive}(Appears from Ante 8)"
-       ],
-       image_url: "img/violet_vessel.png",
-       rarity: "Showdown"
-     },
 ]
-
 
 let shop_items = []
 
